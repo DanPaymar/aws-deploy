3cf4cd904da43958d9d5eb6c160dd7aacf77c4e7
arcticchimpanzee <152743640+arcticchimpanzee@users.noreply.github.com>
2024-03-27 13:43:20 -0700

    removed bs module

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 //
 const express = require('express')
-const bootstrap = require('bootstrap')
-const bootstrap = bootstrap()
+
 const app = express()
 app.use(express.static(`${__dirname}/public`))
 app.listen(4000, () => console.log('running on 4000'))
